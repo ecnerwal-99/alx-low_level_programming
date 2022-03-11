e035f02e963ab2d36d9ae8f8558cc12fc9facb60
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-11 05:59:33 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Description: using the printf function 
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
