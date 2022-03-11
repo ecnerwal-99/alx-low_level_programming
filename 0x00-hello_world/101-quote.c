2545f59f8d24e328d19c80bba7ccb741c05f579e
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-11 07:54:09 -0800

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: real low level stuff
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	return (1);
+}
