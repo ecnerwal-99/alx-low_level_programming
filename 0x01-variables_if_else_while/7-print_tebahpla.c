44db458df44cd50bcb985fd8102fdfe9f469efc3
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-11 14:17:56 -0800

    the rest of the mandatory tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowCase;
+
+	for (lowCase = 'z'; lowCase >= 'a'; lowCase--)
+		putchar(lowCase);
+		putchar('\n');
+
+	return (0);
+}
