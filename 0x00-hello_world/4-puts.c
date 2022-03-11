39afc357581db6459a1847c9603d4c5f26a93ecd
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-11 04:53:20 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * simple staff
+ */
+int main(void)
+{
+	char strg0[]="\"Programming is like building a multilingual puzzle";
+	char strg1[]="";
+	puts(strg0);
+	puts(strg1);
+	return (0);
+}
