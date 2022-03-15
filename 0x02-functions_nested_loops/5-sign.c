57a8c4818ed6501966a876e68e89519bc67adbf7
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-15 12:29:05 -0700

    task 4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		i = 1;
-		_putchar = ('+');
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		i == 0;
-		_putchar = ('0');
+		_putchar('0');
 	}
 	else
 	{
 		i == -1;
-		_putchar = ('-');
+		_putchar('-');
 	}
 	return (i);
 }
