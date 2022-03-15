23a607ae5c5c741f1ba8e375dc77fc523aaa6248
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-15 04:58:45 -0700

    a string is an array of characters

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -  the entry to the program
+ *
+ * Description : program prints out string of characters using putchar function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		putchar(word[i]);
+	}
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this directory has the functions and nested loops projects as i continue learning the c language.
+
