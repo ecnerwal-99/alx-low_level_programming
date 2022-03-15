9d79d5f6bcf0a6f5d70f35addd457bcd9b9d6812
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-15 13:04:34 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - check the code
+ * @n: is parameter of type int
+ * Descriptiom: prints the last digit of a number
+ * Return:0
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
