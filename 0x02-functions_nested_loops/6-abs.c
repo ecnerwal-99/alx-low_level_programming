52f0e2246a7fa5812d9198c2a8a597371c48e038
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-15 12:45:35 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - check the code
+ * int is the type of patameter
+ * Description: absolute
+ * Return: 0
+ */
+int _abs(int m)
+{
+	if (m >= 0)
+		return (m);
+	else
+		return (m * -1);
+}
