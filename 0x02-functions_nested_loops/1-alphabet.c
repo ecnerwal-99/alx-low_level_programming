58c3cf745fc7fa9619e9eec9ffeb1a37300f7c79
ecnerwal-99 <laurentiuslawrence@gmail.com>
2022-03-15 06:30:37 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * prototype: print_alphabet
+ *
+ * Description: functions stuff
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+	return;
+}
