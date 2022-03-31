97eca69272b9663ad03d16d4ad029224ce3af331
lawrence <laurentiuslawrence@gmail.com>
2022-03-31 09:16:17 +0300

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * @brief 
+ * 
+ */
+int _strlen_recursion(char *s)
+{
+    if(*s == '\0')
+    {
+        return;
+    }
+    return (_strlen_recursion(s + 1));
+}
