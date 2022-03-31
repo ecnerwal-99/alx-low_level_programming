4f0ce55d705eb5e4f49dfcb5790e70b0606d63a3
lawrence <laurentiuslawrence@gmail.com>
2022-03-31 08:55:23 +0300

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * @brief 
+ * 
+ */
+void _print_rev_recursion(char *s)
+{
+    if(*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
+}
